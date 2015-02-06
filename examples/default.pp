aa3b69eff0b38fed09f694714204d00a4e46c6e3
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-02-06 17:48:45 +0100

    Fixed default example

--- examples/default.pp
+++ examples/default.pp
@@ -1,5 +1,5 @@
 #
-# sudo puppet apply /vagrant/modules/puppet-php5/example/default.pp
+# sudo puppet apply /vagrant/modules/puppet-php5/examples/default.pp
 # sudo puppet apply -e 'include php5'
 #
 
