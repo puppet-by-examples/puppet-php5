294986ffb7bb5e53f9803f2dcc303896c290b616
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-02-16 17:35:00 +0100

    Prerequisites and validate_platform()
    Fix #14

---
+++ manifests/prerequisites.pp
@@ -0,0 +1,44 @@
+# == Class: php5::prerequisites
+#
+# Ensure any required dependencies for php5 are present.
+#
+# Parameters:
+#
+# None
+#
+
+class php5::prerequisites {
+
+    include stdlib
+
+    if (
+        ($::operatingsystem == "Ubuntu") and
+        ($::operatingsystemrelease == "14.04")
+    ) {
+
+        $deps = [
+            'software-properties-common',
+            'curl',
+            'libcurl3',
+            'libcurl3-dev',
+            'acl'
+        ]
+
+    } elsif (
+        ($::operatingsystem == "Ubuntu") and
+        ($::operatingsystemrelease == "12.04")
+    ) {
+
+        $deps = [
+            'python-software-properties',
+            'curl',
+            'libcurl3',
+            'libcurl3-dev',
+            'acl'
+        ]
+
+    }
+
+    ensure_packages($deps)
+
+}
